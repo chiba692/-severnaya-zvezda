bfa086ebcbc7479f7d6fc308c05627406ac30266
chiba692 <153432199+chiba692@users.noreply.github.com>
2026-09-19 23:19:43 +0300

    Update get-vapid-public.js

--- netlify/functions/get-vapid-public.js
+++ netlify/functions/get-vapid-public.js
@@ -6,7 +6,10 @@ exports.handler = async () => {
       'Cache-Control': 'no-store'
     },
     body: JSON.stringify({
-      VAPID_PUBLIC_KEY: !!process.env.VAPID_PUBLIC_KEY,
+      VAPID_PUBLIC_KEY_EXISTS: typeof process.env.VAPID_PUBLIC_KEY,
+VAPID_PUBLIC_KEY_LENGTH: process.env.VAPID_PUBLIC_KEY
+  ? process.env.VAPID_PUBLIC_KEY.length
+  : 0,
       VAPID_PRIVATE_KEY: !!process.env.VAPID_PRIVATE_KEY,
       SUPABASE_URL: !!process.env.SUPABASE_URL,
       SUPABASE_SECRET_KEY: !!process.env.SUPABASE_SECRET_KEY,
