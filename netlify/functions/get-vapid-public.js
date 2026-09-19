7ca79389e5a7cbd6a36c585e74a59167119520db
chiba692 <153432199+chiba692@users.noreply.github.com>
2026-09-19 22:01:59 +0300

    Update get-vapid-public.js

--- netlify/functions/get-vapid-public.js
+++ netlify/functions/get-vapid-public.js
@@ -1,28 +1,16 @@
-exports.handler = async (event) => {
-  if (event.httpMethod !== 'GET') {
-    return {
-      statusCode: 405,
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({error: 'Method not allowed'})
-    };
-  }
-
-  const publicKey = process.env.VAPID_PUBLIC_KEY;
-
-  if (!publicKey) {
-    return {
-      statusCode: 500,
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({error: 'VAPID public key is missing'})
-    };
-  }
-
+exports.handler = async () => {
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
       'Cache-Control': 'no-store'
     },
-    body: JSON.stringify({publicKey})
+    body: JSON.stringify({
+      VAPID_PUBLIC_KEY: !!process.env.VAPID_PUBLIC_KEY,
+      VAPID_PRIVATE_KEY: !!process.env.VAPID_PRIVATE_KEY,
+      SUPABASE_URL: !!process.env.SUPABASE_URL,
+      SUPABASE_SECRET_KEY: !!process.env.SUPABASE_SECRET_KEY,
+      TEST_PUSH: !!process.env.TEST_PUSH
+    })
   };
 };
