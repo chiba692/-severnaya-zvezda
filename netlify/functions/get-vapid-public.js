791c2bed648b3a63ada3e3750ad117016056ceb7
chiba692 <153432199+chiba692@users.noreply.github.com>
2026-09-19 23:27:50 +0300

    Update get-vapid-public.js

--- netlify/functions/get-vapid-public.js
+++ netlify/functions/get-vapid-public.js
@@ -1,4 +1,19 @@
 exports.handler = async () => {
+  const publicKey = process.env.VAPID_PUBLIC_KEY;
+
+  if (!publicKey) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-store'
+      },
+      body: JSON.stringify({
+        error: 'VAPID public key is missing'
+      })
+    };
+  }
+
   return {
     statusCode: 200,
     headers: {
@@ -6,14 +21,7 @@ exports.handler = async () => {
       'Cache-Control': 'no-store'
     },
     body: JSON.stringify({
-      VAPID_PUBLIC_KEY_EXISTS: typeof process.env.VAPID_PUBLIC_KEY,
-VAPID_PUBLIC_KEY_LENGTH: process.env.VAPID_PUBLIC_KEY
-  ? process.env.VAPID_PUBLIC_KEY.length
-  : 0,
-      VAPID_PRIVATE_KEY: !!process.env.VAPID_PRIVATE_KEY,
-      SUPABASE_URL: !!process.env.SUPABASE_URL,
-      SUPABASE_SECRET_KEY: !!process.env.SUPABASE_SECRET_KEY,
-      TEST_PUSH: !!process.env.TEST_PUSH
+      publicKey
     })
   };
 };
